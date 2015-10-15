b94e4344611f05783af53f27ff1154aa04266120
yurayli <radream61@gmail.com>
2015-10-15 15:21:37 +0800

    make code more readable

--- plot2.R
+++ plot2.R
@@ -1,9 +1,12 @@
 # Import the data
 dat <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+
 # Retrieve the dataset for only 2007-2-1 and 2007-2-2
 manip <- subset(dat, Date == "01/02/2007" | Date == "02/02/2007")
+
 # Retrieve the subset "Global Active Power"
 gap <- as.numeric(levels(manip$Global_active_power))[manip$Global_active_power]
+
 # Draw the graph
 num <- length(gap)
 plot(1:num, gap, type = "l", xaxt = "n", xlab = " ", 
--- plot3.R
+++ plot3.R
@@ -1,11 +1,14 @@
 # Import the data
 dat <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+
 # Retrieve the dataset for only 2007-2-1 and 2007-2-2
 manip <- subset(dat, Date == "01/02/2007" | Date == "02/02/2007")
+
 # Retrieve the subsets "Sub_metering1, 2, 3"
 sm1 <- as.numeric(levels(manip$Sub_metering_1))[manip$Sub_metering_1]
 sm2 <- as.numeric(levels(manip$Sub_metering_2))[manip$Sub_metering_2]
 sm3 <- manip$Sub_metering_3
+
 # Draw the plot
 num <- length(sm1)
 plot(1:num, sm1, type = "l", xaxt = "n", xlab = " ", 
